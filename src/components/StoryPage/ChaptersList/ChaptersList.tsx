1764a40b3761e6fa6989535e340593e1f2593e8f
alan.nns <alan.nns.14.11@gmail.com>
2024-04-19 09:38:01 +0700

    update: add sort to chapterlist

--- src/components/StoryPage/ChaptersList/ChaptersList.tsx
+++ src/components/StoryPage/ChaptersList/ChaptersList.tsx
@@ -5,9 +5,16 @@ import { ChaptersResponse } from '@/types/chapterType'
 import { Story } from '@/types/storyType'
 import { useQuery } from '@tanstack/react-query'
 import { CircleDollarSign } from 'lucide-react'
-import { FC, memo } from 'react'
+import { FC, memo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 type ChaptersListProp = {
   story: Story
@@ -15,13 +22,16 @@ type ChaptersListProp = {
 
 const ChaptersList: FC<ChaptersListProp> = memo(({ story }) => {
   const { t } = useTranslation(['cms'])
+
+  const [chapterSort, setChapterSort] = useState(ChapterSortEnum.LAST)
+
   const { data: chaptersResponse, isSuccess } = useQuery({
-    queryKey: [ChapterKey, story.slug, story.id],
+    queryKey: [ChapterKey, story.slug, story.id, chapterSort],
     queryFn: () => {
       return StoryServices.chapters({
         storySlug: story.slug,
         storyId: `${story.id}`,
-        order: ChapterSortEnum.LAST,
+        order: chapterSort,
       })
     },
   })
@@ -30,6 +40,28 @@ const ChaptersList: FC<ChaptersListProp> = memo(({ story }) => {
 
   return (
     <>
+      <div className="flex justify-end">
+        <Select
+          onValueChange={(value) => {
+            setChapterSort(value as ChapterSortEnum)
+            console.log('change', value)
+          }}
+          value={chapterSort}
+        >
+          <SelectTrigger className="mb-3 w-32">
+            <SelectValue
+              placeholder={ChapterSortEnum.allNames()[chapterSort]}
+            />
+          </SelectTrigger>
+          <SelectContent>
+            {Object.keys(ChapterSortEnum.allNames()).map((chapterSortKey) => (
+              <SelectItem value={chapterSortKey} key={chapterSortKey}>
+                {t<any, {}, null>(ChapterSortEnum.allNames()[chapterSortKey])}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
       <div className="border p-4 rounded-sm h-[500px] overflow-y-auto">
         {isSuccess &&
           chapters.map((chapter) => (
